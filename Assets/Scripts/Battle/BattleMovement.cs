f65b59b90e03882b6ff36b622bebbba618aa5d81
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2020-06-29 14:15:05 +0200

    implemented recalculating path in battleMovement, created static utils

--- Assets/Scripts/Battle/BattleMovement.cs
+++ Assets/Scripts/Battle/BattleMovement.cs
@@ -1,27 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
 
 public class BattleMovement
 {
-  private static List<Tile> _path = new List<Tile>();
+  private static LinkedList<Tile> _path = new LinkedList<Tile>();
+  private static int _cost = 0;
 
   public static void ResetPath()
   {
     _path.Clear();
+    _cost = 0;
   }
+
 
   //PRE: tile is in range of unit
   public static void AddTile(Tile tile, Unit unit)
   {
-    if(unit.Movement < _path.Count + 1)
+    if(unit.Movement < _cost + tile.Cost)
     {
+      HidePath();
       RecalculatePath(unit, tile);
     } else
     {
-      _path.Add(tile);
+      _path.AddLast(tile);
+      _cost += tile.Cost;
     }
   }
+
   
   public static void ShowPath()
   {
@@ -31,6 +37,7 @@ public class BattleMovement
     }
   }
 
+
   public static void HidePath()
   {
     foreach (Tile tile in _path)
@@ -38,6 +45,7 @@ public class BattleMovement
       tile.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
     }
   }
+
 
   public static ISet<Tile> FindViableMoves(Unit unit)
   {
@@ -79,10 +87,50 @@ public class BattleMovement
     return result;
   }
 
-  private static void RecalculatePath(Unit unit, Tile tile)
+  //PRE: tile is in reach of unit
+  private static void RecalculatePath(Unit unit, Tile targetTile)
   {
-    //TODO: implement recalculating path
-    HidePath();
+    //nulls in stack represent going back in path
+    Stack<Tile> stack = new Stack<Tile>();
+    Dictionary<Tile, int> distances = new Dictionary<Tile, int>();
+
     ResetPath();
+    Tile curr = unit.OccupiedTile;
+    distances.Add(curr, 0);
+    _path.AddLast(curr);
+    StaticUtils.PushArray(stack, curr.GetAdjacentTiles());
+
+    while(curr != targetTile)
+    {
+      stack.Push(null);
+
+      foreach(Tile tile in curr.GetAdjacentTiles())
+      {
+        if (!distances.ContainsKey(tile))
+        {
+          distances.Add(tile, _cost + tile.Cost);
+          stack.Push(tile);
+        } else if (tile.Cost + _cost < distances[tile])
+        {
+          distances[tile] = tile.Cost + _cost;
+          stack.Push(tile);
+        }
+      }
+
+      curr = stack.Pop();
+      while(curr == null || _cost + curr.Cost > unit.Movement)
+      {
+        if (curr == null)
+        {
+          _cost -= _path.Last.Value.Cost;
+          _path.RemoveLast();
+        }
+
+        curr = stack.Pop();
+      }
+
+      _path.AddLast(curr);
+      _cost += curr.Cost;
+    }
   }
 }
---
+++ Assets/Scripts/StaticUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class StaticUtils
+{
+  //returns false when stack doesn't change
+  public static bool PushCollaction<T>(Stack<T> stack, ICollection<T> items)
+  {
+    if(items.Count == 0)
+    {
+      return false;
+    }
+
+    foreach(T item in items)
+    {
+      stack.Push(item);
+    }
+
+    return true;
+  }
+
+  //returns false when stack doesn't change
+  public static bool PushArray<T>(Stack<T> stack, T[] items)
+  {
+    if(items.Length == 0)
+    {
+      return false;
+    }
+
+    foreach(T item in items)
+    {
+      stack.Push(item);
+    }
+
+    return true;
+  }
+}
