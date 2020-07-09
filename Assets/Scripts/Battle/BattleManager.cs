e46a4895337f7466d217a0585db1563f4acb2f4e
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2020-07-09 18:16:36 +0200

    Added array of all map tiles to BatlleManager

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 
@@ -6,9 +8,12 @@ public class BattleManager : MonoBehaviour
 {
   public const int TILES_LAYER = 1 << 8;
   public const string UNIT_TAG = "Unit";
+  public const string TILE_TAG = "Tile";
 
   public static GameObject DefaultOverlay;
   public GameObject CursorObject;
+  public Tile[,] MapTiles { get; private set; }
+  [SerializeField] public int TilesDistance { get; private set; }
 
   public Cursor Cursor { get; private set; }
   public OverlaysManager OverlaysManager { get; } = new OverlaysManager();
@@ -21,12 +26,12 @@ public class BattleManager : MonoBehaviour
     BattleTurnManager = GetComponent<BattleTurnManager>();
     Cursor = CursorObject.GetComponent<Cursor>();
     DefaultOverlay = Cursor.Overlay;
+    FindAllTiles();
   }
 
   // Start is called before the first frame update
   void Start()
   {
-
   }
 
   private void FixedUpdate()
@@ -39,6 +44,34 @@ public class BattleManager : MonoBehaviour
     
     
     
+  }
+
+  //initialises and populates MapTiles [0,0] is in the left bottom corner
+  //i.e minZ, minX
+  private void FindAllTiles()
+  {
+    GameObject[] tileObjects = GameObject.FindGameObjectsWithTag(TILE_TAG);
+    int minZ = int.MaxValue;
+    int maxZ = int.MinValue;
+    int minX = int.MaxValue;
+    int maxX = int.MinValue;
+
+    foreach (GameObject tileObject in tileObjects)
+    {
+      minZ = Math.Min(minZ, (int)tileObject.transform.position.z);
+      maxZ = Math.Max(maxZ, (int)tileObject.transform.position.z);
+      minX = Math.Min(minX, (int)tileObject.transform.position.x);
+      maxX = Math.Max(maxX, (int)tileObject.transform.position.x);
+    }
+
+    MapTiles = new Tile[maxZ - minZ + 1, maxX - minX + 1];
+
+    foreach (GameObject tileObject in tileObjects)
+    {
+      Tile tile = tileObject.GetComponent<Tile>();
+      MapTiles[(int)tile.transform.position.z - minZ,
+               (int)tile.transform.position.x - minX] = tile;
+    }
   }
 
 }
