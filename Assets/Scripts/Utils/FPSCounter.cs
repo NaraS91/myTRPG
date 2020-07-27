b8879ce094ad60fe7aa32229f444dbecefdfefe5
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2020-07-27 10:26:28 +0200

    implemented fps counter

---
+++ Assets/Scripts/Utils/FPSCounter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+  [SerializeField] private Text _text;
+
+  // Update is called once per frame
+  void Update()
+  {
+    int fps = (int)(1 / Time.unscaledDeltaTime);
+    _text.text = "FPS " + fps;
+  }
+}
